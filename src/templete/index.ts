f40abf50945cbc0027410b480baa9b5953da7fa0
chunyang.chen <chunyang.chen@iglooinsure.com>
2024-02-01 18:12:18 +0800

    feat: 更新react模版

--- src/templete/index.ts
+++ src/templete/index.ts
@@ -1,21 +1,38 @@
 export default class CompTemplete {
-  key: string = "";
+  styleWrapKey: string = "";
+  componentName: string = "";
+  interfaceName: string = "";
 
   constructor(v: string) {
-    this.key = `${v}Wrap`;
+    this.styleWrapKey = `${v}Wrapper`;
+    this.componentName =
+      v.substring(0, 1).toUpperCase() + v.substring(1, v.length);
+    this.interfaceName = `${this.componentName}Props`;
   }
 
   generateTsx() {
-    return `import styles from "./index.less";
+    return `import React from "react";
+import styles from "./index.less";
 
-export default () => {
-  return <div className={styles.${this.key}}>init</div>;
+interface ${this.interfaceName} {
+  name: string;
+}
+
+/**
+ * 这是一个初始化的组件
+ * @param {string} name - 姓名
+ * @returns 返回一个jsx组件
+ */
+const ${this.componentName}: React.FC<${this.interfaceName}> = ({ name }) => {
+  return <div className={styles.${this.styleWrapKey}}>init</div>;
 };
+
+export default ${this.componentName};
 `;
   }
 
   generateLess() {
-    return `.${this.key} {
+    return `.${this.styleWrapKey} {
   
 }`;
   }
